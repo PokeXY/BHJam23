85fb142693f253c81884a939b5aecb6e6d0320e9
Tanner Olsen <tanner-olsen@hotmail.com>
2023-05-07 12:57:04 -0400

    Added in enemy movement + brighter leaves!

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
+// Basic movement code courtesy of Alexander Zotov, May 18, 2021.
+// https://www.youtube.com/watch?v=NbA95f1FlXQ
+
 public class EnemyController : MonoBehaviour
 {
-
     public float hitpoints;
     public float maxHitpoints;
     public TextMeshProUGUI healthUI;
     public HitpointBar hitpointBar;
+    private float directionX;
+    public float moveSpeed;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,6 +23,12 @@ public class EnemyController : MonoBehaviour
         hitpoints = maxHitpoints;
         healthUI.text = "HP: " + hitpoints.ToString();
         hitpointBar.SetHealth(hitpoints, maxHitpoints);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        directionX = -1.0f;
     }
 
     public void TakeHit(float damage)
@@ -35,5 +46,19 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            directionX *= -1.0f;
+        }
+
     }
 }
